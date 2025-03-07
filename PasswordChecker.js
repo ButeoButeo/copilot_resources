992ce1cf3b6773990cc6e8319bb988e9f21121d5
Tânia P <taniapestana@gmail.com>
2025-03-07 08:13:56 +0000

    Update PasswordChecker.js

--- PasswordChecker.js
+++ PasswordChecker.js
@@ -2,4 +2,7 @@ export function checkPassword(password) {
   if(typeof password !== 'string') {
     throw new Error('Password must be a string');
   }
+  if(password.length < 8) {
+    throw new Error('Password must be at least 8 characters long');
+  }
 }
