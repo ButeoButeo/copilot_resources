ba9b61c95eca37658d90e457d2133cfe44ee07e5
Tânia P <taniapestana@gmail.com>
2025-03-07 08:34:39 +0000

    Update PasswordChecker.js

--- PasswordChecker.js
+++ PasswordChecker.js
@@ -1,3 +1,16 @@
+/**
+ * Checks if the provided password meets the required criteria.
+ *
+ * @param {string} password - The password to be checked.
+ * @throws {Error} If the password is not a string.
+ * @throws {Error} If the password is less than 8 characters long.
+ * @throws {Error} If the password is more than 20 characters long.
+ * @throws {Error} If the password does not contain at least one lowercase letter.
+ * @throws {Error} If the password does not contain at least one uppercase letter.
+ * @throws {Error} If the password does not contain at least one number.
+ * @throws {Error} If the password does not contain at least one special character (!@#$%^&*).
+ * @returns {boolean} Returns true if the password meets all criteria.
+ */
 export function checkPassword(password) {
   if(typeof password !== 'string') {
     throw new Error('Password must be a string');
